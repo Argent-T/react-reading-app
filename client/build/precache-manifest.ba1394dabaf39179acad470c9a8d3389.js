a04945cc7a709e798f9834daa13c903d03db193d
Thomas Argent <tfargent@gmail.com>
2020-03-10 21:32:48 -0400

    built

---
+++ client/build/precache-manifest.ba1394dabaf39179acad470c9a8d3389.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13edd92f87dbb43d20bd",
+    "url": "/static/css/main.96489efd.chunk.css"
+  },
+  {
+    "revision": "13edd92f87dbb43d20bd",
+    "url": "/static/js/main.0cdc5b6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c80513fd735037d0cb2f",
+    "url": "/static/js/2.665bd340.chunk.js"
+  },
+  {
+    "revision": "76e29b5c15e5029c397bc5b7e435cf26",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://heroku_fl9wd3f1:mongo123@ds033196.mlab.com:33196/heroku_fl9wd3f1");
 
 // Start the API server
 app.listen(PORT, function() {
